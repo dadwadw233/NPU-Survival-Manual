872485155b338b7d5de0ad6e1a0655499b0474ad
dadwadw233 <913217005@qq.com>
2022-12-17 17:00:41 +0800

    解决跨域问题

---
+++ src/main/java/com/yyh/nwpusurvivalmanual/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.yyh.nwpusurvivalmanual.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //跨域配置
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
